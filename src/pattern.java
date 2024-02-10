a4f73c8174feaf074f2d20f364f0ab7b007f0dd3
subrato <subratogaur@gmail.com>
2024-02-10 18:45:35 +0530

    pattern

--- src/pattern.java
+++ src/pattern.java
@@ -1,4 +1,154 @@
-public class pattern {
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=5;j++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                             pattern 2
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                    pattern 3
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=5;j>=i;j--)
+//            {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                          pattern4
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                        pattern5
 
 
-}
+//public class pattern {
+//    public static void main(String args[]) {
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                    System.out.print("*");
+//
+//            }
+//            System.out.println();
+//        }
+//        for (int i = 1; i <= 4; i++) {
+//            for (int j = 4; j >= i; j--) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+//}
+
+//                              pattern6
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=i;j<=5;j++)
+//            {
+//                System.out.print(" ");
+//            }
+//            for(int k=1;k<=i;k++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                        pattern7
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=1;j<=i;j++)
+//            {
+//                System.out.print(" ");
+//            }
+//            for(int k=5;k>=i;k--){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//    }
+//
+//}
+
+//                          pattern8
+
+//public class pattern {
+//    public static void main(String args[]){
+//        for(int i=1;i<=5;i++)
+//        {
+//            for(int j=5;j>=i;j--)
+//            {
+//                System.out.print(" ");
+//            }
+//            for(int k=1;k<=i;k++){
+//                System.out.print("*");
+//            }
+//          if(i>=2)
+//          {
+//              for(int m=2;m<=i;m++)
+//              {
+//                  System.out.print("*");
+//              }
+//          }
+//
+//            System.out.println();
+//
+//        }
+//    }
+//
+//}
+
+
+//                    pattern9
+
+
