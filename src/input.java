7889447ab0573af25ce8c9756136741b05378343
subrato <subratogaur@gmail.com>
2024-02-14 23:44:03 +0530

    taking input int type

---
+++ src/input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        System.out.println("Enter the number : ");
+        Scanner Sc = new Scanner(System.in);
+        int num_1 = Sc.nextInt();
+        System.out.println(num_1);
+    }
+}
