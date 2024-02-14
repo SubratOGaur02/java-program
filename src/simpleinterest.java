ea30235d62bb2727760e15a60f3860b5148ede62
subrato <subratogaur@gmail.com>
2024-02-15 00:23:18 +0530

    calculate the simple interest

---
+++ src/simpleinterest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class simpleinterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the principle: ");
+        float p = sc.nextFloat();
+
+        System.out.println("Enter the rate of interest: ");
+        float r = sc.nextFloat();
+
+        System.out.println("Enter the time: ");
+        float t = sc.nextFloat();
+
+        float si = p*r*t/100;
+        System.out.println("The simple interest is: "+si);
+
+    }
+}
