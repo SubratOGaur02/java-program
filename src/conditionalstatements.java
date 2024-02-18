8499de82c2d40a244fac4c11bd9f8dce96fe50f0
subrato <subratogaur@gmail.com>
2024-02-18 14:56:23 +0530

    conditionalstatemnts

--- src/conditionalstatements.java
+++ src/conditionalstatements.java
@@ -1,20 +1,129 @@
+import java.lang.runtime.SwitchBootstraps;
 import java.util.Scanner;
 
-// Print "odd" if te in put value is odd, otherwsie print"even".
-//Note input value is between 1 and 10^6.
+
 public class conditionalstatements {
-    public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
-        System.out.println("Enter the number: ");
-        int n = Sc.nextInt();
+
+//Print "odd" if te in put value is odd, otherwsie print"even".
+//Note input value is between 1 and 10^6.
+
+//    public static void main(String[] args) {
+//        Scanner Sc = new Scanner(System.in);
+//        System.out.println("Enter the number: ");
+//        int n = Sc.nextInt();
+//
+//
+//        if (n%2==1){
+//            System.out.println("odd number");
+//
+//        }
+//        else {
+//            System.out.println("even number");
+//        }
+//    }
+//    ternary operator
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("enter the age");
+//        int n = sc.nextInt();
+//        String age;
+//        age = (n%2==0)? "even" : "odd";
+//        System.out.println(age);
+//
+//
+//}
+
+    /////////////////////////////////////////////////////////////////////////////////////////////////
+
+//WAP to divide people into 3 age groups depending upon their age
+//      Age         |      Output
+//  below 12        |      child
+//between 12 and 18 |      teenager
+//above 18          |      adult
+
+//    public static void main(String[] args) {
+//        Scanner Sc = new Scanner(System.in);
+//        System.out.println("Enter the age: ");
+//        int n = Sc.nextInt();
+//        if (n<=12){
+//            System.out.println("Child");
+//
+//        }
+//        else if (n>12 && n<18){
+//            System.out.println("Teenager");
+//        }
+//        else {
+//            System.out.println("Adult");
+//        }
+//    }
 
 
-        if (n%2==1){
-            System.out.println("odd number");
+//    ////////////////////////////////////////////////////////////////////////////////////////////
 
-        }
-        else {
-            System.out.println("even number");
-        }
-    }
+
+//    WAP a program to print the value if it is even and divisible by 3.
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("enter the value");
+//    int n = sc.nextInt();
+//    if (n%2==0 && n%3==0){
+//        System.out.println("Found ans -" + n);
+//    }
+//
+//}
+    ///////////////////////////////////////////////////////
+
+//    Wap to print the value if it is divisible by 3 or 5.
+//public static void main(String[] args) {
+//    Scanner sc = new Scanner(System.in);
+//    System.out.println("enter the number: ");
+//    int n = sc.nextInt();
+//    if (n%3==0 || n%5==0){
+//        System.out.println("Found ans-"+n);
+//    }
+//}
+
+//    /////////////////////////////////////////////////////////////////////////////////
+
+// ///////----------------   switch case     --------------------//////
+
+//    public static void main(String[] args) {
+//        Scanner sc =new Scanner(System.in);
+//        System.out.println("Enter the day: ");
+//        int Day = sc.nextInt();
+//        switch (Day) {
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//                break;
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//            case 7:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Invalid day no.");
+//
+//        }
+//
+//    }
+
+
+//    /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
 }
